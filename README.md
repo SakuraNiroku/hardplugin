440f59c85a2625230ad524043b9563758f6d3c46
LiangYin <liangyin17@outlook.com>
2023-12-10 07:43:36 +0800

    rm fix

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # HardPlugin
 这个插件让minecraft更困难了（可能？
 
-* 插件版本：spigot 1.20.2+
-* 依赖：de.tr7zw.item-nbt-api
+* 插件版本：spigot 1.20.2
+* 依赖：
+  * de.tr7zw.item-nbt-api
   * 没有了qwq
-* 测试服务器？
-  * 没有，要不你送我？bushi
-## 有好想法？
-* 提交issue就行qwq，记得主题前面加上“[想法]”
---
+++ src/main/java/cn/catver/plugins/hardplugin/manyblock/tools/BlockMatchOfManyBlock.java
@@ -0,0 +1,12 @@
+package cn.catver.plugins.hardplugin.manyblock.tools;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BlockMatchOfManyBlock {
+    HashMap<Location, Material> locationWhatMaterial = new HashMap<>();
+
+}
